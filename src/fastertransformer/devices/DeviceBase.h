73f7192225ec7d3bf57246b055f3b11e89d1957d
zjli2013 <leezhengjiang@gmail.com>
2024-06-11 20:55:15 +0800

    add back device-special implements

--- src/fastertransformer/devices/DeviceBase.h
+++ src/fastertransformer/devices/DeviceBase.h
@@ -22,9 +22,9 @@ public:
 public:
     // device-independence op implementations
     CloneOutput clone(const CloneParams& params) override;
-//     AttentionLayerOutput attentionLayer(const AttentionLayerParams& params) override;
-//     FfnLayerOutput ffnLayer(const FfnLayerParams& params) override;
-//     LoraLinearOutput loraLinear(const LoraLinearParams& params) override;
+    AttentionLayerOutput attentionLayer(const AttentionLayerParams& params) override;
+    FfnLayerOutput ffnLayer(const FfnLayerParams& params) override;
+    LoraLinearOutput loraLinear(const LoraLinearParams& params) override;
 
 protected:
     BufferStatus queryBufferStatus();
